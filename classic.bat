e13d1f7e61ed79c41fa5d531e09d6b3e7be98c85
markus_hahn <markus_hahn>
2004-12-08 06:59:49 +0000

    forgot to add the Eclipse project files, added unzip view after packing

--- classic.bat
+++ classic.bat
@@ -1,9 +1,10 @@
 @echo off
 REM batch file to create a classic ZIP file for distribution on <http://www.hotpixel.net/software.html>
 REM * usage: classic.bat {3-digit-version}
-REM * make sure to have md5cksum.exe in your path
+REM * make sure to have md5cksum.exe and unzip.exe in your path
 REM * ZIP file gets created in the system's temporary folder
 REM * old ZIP file(s) will be deleted
 del %TEMP%\bfj%1.zip
-zip -9 %TEMP%\bfj%1.zip src\java\net\sourceforge\blowfishj\*.java src\test\java\test\net\sourceforge\blowfishj\*.java license.txt readme.txt
-md5cksum %TEMP%\bfj%1.zip
+zip -9 %TEMP%\bfj%1.zip src\java\net\sourceforge\blowfishj\*.java src\test\java\test\net\sourceforge\blowfishj\*.java license.txt readme.txt .classpath .project
+unzip -v %TEMP%\bfj%1.zip
+start md5cksum %TEMP%\bfj%1.zip
